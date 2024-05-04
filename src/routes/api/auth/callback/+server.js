c2c355ed82ba41a33102512406e64270ee6f3c28
kaushalluffa <kaushalluffa91@gmail.com>
2024-05-04 18:22:36 -0400

    feat(spotify-auth): added access token and refresh token to cookies

--- src/routes/api/auth/callback/+server.js
+++ src/routes/api/auth/callback/+server.js
@@ -1,3 +1,35 @@
-export const GET = async (event) => {
-	console.log(event);
+import { error, redirect } from '@sveltejs/kit';
+import { SPOTIFY_APP_CLIENT_ID, SPOTIFY_APP_CLIENT_SECRET, BASE_URL } from '$env/static/private';
+export const GET = async ({ url, cookies }) => {
+	const code = url?.searchParams?.get('code') || null;
+	const state = url?.searchParams?.get('state') || null;
+	const storedSate = cookies.get('spotify_auth_state') || null;
+	const storedChallengeVerifier = cookies.get('spotify_auth_challenge_verify') || null;
+	if (state === null || state !== storedSate) {
+		throw error(400, 'State mismatch');
+	}
+	const response = await fetch('https://accounts.spotify.com/api/token', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			Authorization: `Basic ${Buffer.from(`${SPOTIFY_APP_CLIENT_ID}:${SPOTIFY_APP_CLIENT_SECRET}`).toString('base64')}`
+		},
+		body: new URLSearchParams({
+			grant_type: 'authorization_code',
+			code: code || '',
+			redirect_uri: `${BASE_URL}/api/auth/callback`,
+			client_id: SPOTIFY_APP_CLIENT_ID,
+			code_verifier: storedChallengeVerifier || ''
+		})
+	});
+	const data = await response.json();
+	if (data?.error) {
+		throw error(400, data?.error_description || data?.error);
+	}
+	cookies.delete('spotify_auth_state', { path: '/' });
+	cookies.delete('spotify_auth_challenge_verify', { path: '/' });
+	cookies.set('refresh_token', data?.refresh_token, { path: '/' });
+	cookies.set('access_token', data?.access_token, { path: '/' });
+
+	throw redirect(303, '/');
 };
--- src/routes/api/auth/login/+server.js
+++ src/routes/api/auth/login/+server.js
@@ -19,7 +19,9 @@ const scope = `ugc-image-upload user-modify-playback-state user-read-playback-st
 			   user-library-read`;
 const state = generateRandomString(16);
 const challenge = pkce.create();
-export const GET = async () => {
+export const GET = async ({ cookies }) => {
+	cookies.set('spotify_auth_state', state, { path: '/' });
+	cookies.set('spotify_auth_challenge_verify', challenge.code_verifier, { path: '/' });
 	throw redirect(
 		307,
 		`https://accounts.spotify.com/authorize?${new URLSearchParams({ response_type: 'code', client_id: SPOTIFY_APP_CLIENT_ID, scope, redirect_uri: `${BASE_URL}/api/auth/callback`, state, code_challenge_method: 'S256', code_challenge: challenge.code_challenge })}`
