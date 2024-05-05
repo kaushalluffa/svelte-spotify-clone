ca42ced2bcebb0db8ddf68673dba00cdc5e39d7c
kaushalluffa <kaushalluffa91@gmail.com>
2024-05-05 15:41:48 -0400

    feat(navigation): desktop navigation

---
+++ src/lib/components/Navigation.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { Home, Search, ListMusic } from 'lucide-svelte';
+	import logo from '$assets/logo.png';
+	import { page } from '$app/stores';
+	export let desktop;
+	const menuItems = [
+		{ path: '/', label: 'Home', icon: Home },
+		{ path: '/search', label: 'Search', icon: Search },
+		{ path: '/playlists', label: 'Playlists', icon: ListMusic }
+	];
+</script>
+
+<div class="nav-content" class:desktop class:mobile={!desktop}>
+	<nav aria-label="Main">
+		<div class="nav-content-inner">
+			<img src={logo} class="logo" alt="Spotify" width="100px" />
+			<ul>
+				{#each menuItems as item}
+					<li class:active={item?.path === $page?.url?.pathname}>
+						<a href={item?.path}>
+							<svelte:component
+								this={item?.icon}
+								focusable="false"
+								aria-hidden="true"
+								color="var(--text-color)"
+								size={26}
+							/>
+							{item?.label}</a
+						>
+					</li>
+				{/each}
+			</ul>
+		</div>
+	</nav>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,17 @@
 <script>
 	import 'modern-normalize/modern-normalize.css';
 	import '../styles/main.scss';
-
+	import { Navigation } from '$components';
 	export let data;
 	$: user = data?.user;
 </script>
 
 <div id="main">
+	{#if user}
+		<div class="sidebar">
+			<Navigation desktop={true} />
+		</div>
+	{/if}
 	<div id="content">
 		<main id="main-content">
 			<slot />
@@ -16,7 +21,9 @@
 
 <style lang="scss">
 	#main {
+		display: flex;
 		#content {
+			flex: 1;
 			main#main-content {
 				padding: 30px 15px 60px;
 				@include breakpoint.up('md') {
