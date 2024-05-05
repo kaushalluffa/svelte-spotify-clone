460f761e8ac3d38c63feae29bf48accd4861bc95
kaushalluffa <kaushalluffa91@gmail.com>
2024-05-05 16:00:25 -0400

    style(topbar): styling for topbar nav desktop

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,16 @@
 	import '../styles/main.scss';
 	import { Navigation } from '$components';
 	export let data;
+	let topbar;
+	let scrollY;
+	let headerOpacity = 0;
 	$: user = data?.user;
+	$: if (topbar) {
+		headerOpacity = scrollY / topbar?.offsetHeight < 1 ? scrollY / topbar?.offsetHeight : 1;
+	}
 </script>
+
+<svelte:window bind:scrollY />
 
 <div id="main">
 	{#if user}
@@ -13,7 +21,15 @@
 		</div>
 	{/if}
 	<div id="content">
-		<main id="main-content">
+		<div id="topbar" bind:this={topbar}>
+			<div
+				class="topbar-bg"
+				style:background-color="var(--header-colo)"
+				style:opacity={headerOpacity}
+			/>
+			Topbar
+		</div>
+		<main id="main-content" class:logged-in={user}>
 			<slot />
 		</main>
 	</div>
@@ -24,10 +40,34 @@
 		display: flex;
 		#content {
 			flex: 1;
+			#topbar {
+				position: fixed;
+				height: var(--header-height);
+				padding: 0 15px;
+				display: flex;
+				align-items: center;
+				width: 100%;
+				z-index: 100;
+				.topbar-bg {
+					position: absolute;
+					width: 100%;
+					top: 0;
+					left: 0;
+					height: 100%;
+					z-index: -1;
+				}
+				@include breakpoint.up('md') {
+					padding: 0 30px;
+					width: calc(100% - var(--sidebar-width));
+				}
+			}
 			main#main-content {
 				padding: 30px 15px 60px;
 				@include breakpoint.up('md') {
 					padding: 30px 30px 60px;
+				}
+				&.logged-in {
+					padding-top: calc(30px + var(--header-height));
 				}
 			}
 		}
