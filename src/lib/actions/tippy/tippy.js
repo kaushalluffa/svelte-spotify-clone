e537ab45b6e582007edd82dcef54ea3f9358261d
kaushalluffa <kaushalluffa91@gmail.com>
2024-05-05 20:40:35 -0400

    feat(tippy-accessibility): added tippy accessibility plugins

--- src/lib/actions/tippy/tippy.js
+++ src/lib/actions/tippy/tippy.js
@@ -1,11 +1,14 @@
-import tippy from 'tippy.js';
+import tippy, { hideOnEsc, hideOnPopperBlur, hideOthersOnOpen } from './tippy-plugins.js';
 import 'tippy.js/dist/tippy.css';
 
 export default function (node, options) {
-	const instance = tippy(node, options);
+	const plugins = [hideOnEsc, hideOnPopperBlur, hideOthersOnOpen];
+	const _options = options ? { ...options, plugins } : { plugins };
+	const instance = tippy(node, _options);
 	return {
 		update(newOptions) {
-			instance.setProps(newOptions);
+			const _newOptions = newOptions ? { ...newOptions, plugins } : { plugins };
+			instance.setProps(_newOptions);
 		},
 		destroy() {
 			instance.destroy();
