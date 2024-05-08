10e683dd362d9d2aafa1cf0827bf9bfeb4c98c8b
kaushalluffa <kaushalluffa91@gmail.com>
2024-05-07 22:14:33 -0400

    feat(markup for item page): markup for item page

---
+++ src/lib/components/ItemPage.svelte
@@ -0,0 +1,30 @@
+<script>
+	import { Music } from 'lucide-svelte';
+
+	export let color;
+	export let image;
+	export let title;
+	export let type;
+</script>
+
+<div class="banner">
+	<div class="cover">
+		{#if image}
+			<img src={image} alt={title} />
+		{:else}
+			<div class="cover-placeholder">
+				<Music focusable="false" aria-hidden="true" />
+			</div>
+		{/if}
+	</div>
+	<div class="info">
+		{#if type}
+			<p class="type">{type}</p>
+		{/if}
+		<h1 class="title">{title}</h1>
+		<slot name="meta" />
+	</div>
+</div>
+<div class="content">
+	<slot />
+</div>
--- src/routes/album/[id]/+page.svelte
+++ src/routes/album/[id]/+page.svelte
@@ -1,27 +1,41 @@
 <script>
+	import { ItemPage } from '$components';
 	export let data;
 	import { getCopyrightSymbol } from '$helpers';
 	$: album = data?.album;
 </script>
 
-<h1>{album?.name}</h1>
-<div class="tracks">
-	<ul>
-		{#each album?.tracks?.items as track}
-			<li>{track?.name}</li>
-		{/each}
-	</ul>
-</div>
-<div class="credits">
-	<p class="date">
-		{new Date(album?.release_date)?.toLocaleDateString('en', { dateStyle: 'medium' })}
+<ItemPage
+	type={album?.album_type}
+	title={album?.name}
+	color={undefined}
+	image={album?.images?.length > 0 ? album?.images?.[0]?.url : undefined}
+>
+	<p class="meta" slot="meta">
+		<span class="artists">{album?.artists?.map((item) => item?.name)?.join(', ')}</span>
+		<span class="date">{new Date(album?.release_date)?.getFullYear()}</span>
+		<span class="tracks-count"
+			>{`${album?.total_tracks} Track${album?.total_tracks > 1 ? 's' : ''}`}</span
+		>
 	</p>
-	{#each album?.copyrights as copyright}
-		<p class="copyright">
-			{getCopyrightSymbol(copyright?.type)}{copyright?.text}
+	<div class="tracks">
+		<ul>
+			{#each album?.tracks?.items as track}
+				<li>{track?.name}</li>
+			{/each}
+		</ul>
+	</div>
+	<div class="credits">
+		<p class="date">
+			{new Date(album?.release_date)?.toLocaleDateString('en', { dateStyle: 'medium' })}
 		</p>
-	{/each}
-</div>
+		{#each album?.copyrights as copyright}
+			<p class="copyright">
+				{getCopyrightSymbol(copyright?.type)}{copyright?.text}
+			</p>
+		{/each}
+	</div>
+</ItemPage>
 
 <style lang="scss">
 	.credits {
